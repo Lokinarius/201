baa74be59922b4296fd03a879301eee8e89e9ad6
William Galvão <Liam_draven@outlook.com>
2025-01-20 14:52:53 -0300

    Create Casting.java

---
+++ Conteudo/Java01BASICO/Aula01TiposPrimitivo/Ex02Casting/Casting.java
@@ -0,0 +1,9 @@
+package Projeto201.Conteudo.Java01BASICO.Aula01TiposPrimitivo.Ex02Casting;
+
+public class Casting {
+    public static void main(String[] args) {
+        int inteiro = 100;
+        float real = (float)inteiro; // Casting
+        System.out.println("O número inteiro "+inteiro+" em real é "+real);
+    }
+}
