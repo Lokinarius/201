0a610ad2e9a9afea3b3a0676a01f69738e201bcf
William Galvão <Liam_draven@outlook.com>
2025-01-22 17:30:30 -0300

    Create ParEPositivo.java

---
+++ Conteudo/Java01BASICO/Aula03OperadoresLogicos/Ex02ParEPositivo/ParEPositivo.java
@@ -0,0 +1,30 @@
+package Projeto201.Conteudo.Java01BASICO.Aula03OperadoresLogicos.Ex02ParEPositivo;
+
+import java.util.Scanner;
+
+public class ParEPositivo {
+    public static void main(String[] args) {
+        // ABERTURA DO SCANNER
+        Scanner scanner = new Scanner(System.in);
+
+        // ENTRADA DE DADOS
+        System.out.println("Digite um número: ");
+        int numero = scanner.nextInt();
+
+
+        // VERIFICAÇÃO BOOLEANA
+        boolean positivo = numero > 0;
+        boolean negativo = numero < 0;
+        boolean par = numero %2 == 0;
+
+        // SAÍDA DE DADOS
+        System.out.println(positivo?"O número digitado é positivo":
+                negativo?"O número digitado é negativo":
+                "O número digitado é zero");
+        System.out.println(par?"O número digitado é par":
+                "O número digitado é impar");
+
+        // FECHAMENTO DE SCANNER
+        scanner.close();
+    }
+}
