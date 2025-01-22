99b4fab24bd938cac8481312c90cd652928d681d
William Galvão <Liam_draven@outlook.com>
2025-01-22 17:31:24 -0300

    Create Adulto.java

---
+++ Conteudo/Java01BASICO/Aula03OperadoresLogicos/Ex05Adulto/Adulto.java
@@ -0,0 +1,15 @@
+package Projeto201.Conteudo.Java01BASICO.Aula03OperadoresLogicos.Ex05Adulto;
+
+import java.util.Scanner;
+
+public class Adulto {
+    public static void main(String[] args) {
+        // ABERTURA DO SCANNER
+        Scanner scanner = new Scanner(System.in);
+        // ENTRADA DE DADOS
+        // SAÍDA DE DADOS
+        // FECHAMENTO DE SCANNER
+        scanner.close();
+    }
+
+}
