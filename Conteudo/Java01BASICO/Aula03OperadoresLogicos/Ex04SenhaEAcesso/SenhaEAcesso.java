ee425dcda2876fe2f88adcf0149550f327e89428
William Galvão <Liam_draven@outlook.com>
2025-01-22 17:31:22 -0300

    Create SenhaEAcesso.java

---
+++ Conteudo/Java01BASICO/Aula03OperadoresLogicos/Ex04SenhaEAcesso/SenhaEAcesso.java
@@ -0,0 +1,14 @@
+package Projeto201.Conteudo.Java01BASICO.Aula03OperadoresLogicos.Ex04SenhaEAcesso;
+
+import java.util.Scanner;
+
+public class SenhaEAcesso {
+    public static void main(String[] args) {
+        // ABERTURA DO SCANNER
+        Scanner scanner = new Scanner(System.in);
+        // ENTRADA DE DADOS
+        // SAÍDA DE DADOS
+        // FECHAMENTO DE SCANNER
+        scanner.close();
+    }
+}
