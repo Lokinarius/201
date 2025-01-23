56a549b14ff4d61abb1a349ea995a44a28200ea3
William Galvão <Liam_draven@outlook.com>
2025-01-23 14:56:03 -0300

    Update ParEPositivo.java

--- Conteudo/Java01BASICO/Aula03OperadoresLogicos/Ex02ParEPositivo/ParEPositivo.java
+++ Conteudo/Java01BASICO/Aula03OperadoresLogicos/Ex02ParEPositivo/ParEPositivo.java
@@ -1,6 +1,8 @@
 package Projeto201.Conteudo.Java01BASICO.Aula03OperadoresLogicos.Ex02ParEPositivo;
 
 import java.util.Scanner;
+// O perador ternário é um tipo de condicional mais simples que pode ser extremamente útil, especialmente se feito em
+// uma operação lógica simples?
 
 public class ParEPositivo {
     public static void main(String[] args) {
