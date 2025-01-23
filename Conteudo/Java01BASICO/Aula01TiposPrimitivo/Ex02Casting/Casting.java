a1713d5deaf6b1dbb3b34ddabc34c6a21af988b6
William Galvão <Liam_draven@outlook.com>
2025-01-23 14:55:35 -0300

    Update Casting.java

--- Conteudo/Java01BASICO/Aula01TiposPrimitivo/Ex02Casting/Casting.java
+++ Conteudo/Java01BASICO/Aula01TiposPrimitivo/Ex02Casting/Casting.java
@@ -1,5 +1,6 @@
 package Projeto201.Conteudo.Java01BASICO.Aula01TiposPrimitivo.Ex02Casting;
 // Casting é uma forma de conversão de tipos primitivos, ela pode ser implicita, ou explicita como no caso abaixo
+
 public class Casting {
     public static void main(String[] args) {
         int inteiro = 100;
