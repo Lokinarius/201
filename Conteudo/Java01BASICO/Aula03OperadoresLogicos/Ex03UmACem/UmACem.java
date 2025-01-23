22f68df6327de149cb90c53537653ce812a48a13
William Galvão <Liam_draven@outlook.com>
2025-01-23 14:56:07 -0300

    Update UmACem.java

--- Conteudo/Java01BASICO/Aula03OperadoresLogicos/Ex03UmACem/UmACem.java
+++ Conteudo/Java01BASICO/Aula03OperadoresLogicos/Ex03UmACem/UmACem.java
@@ -1,6 +1,9 @@
 package Projeto201.Conteudo.Java01BASICO.Aula03OperadoresLogicos.Ex03UmACem;
 
 import java.util.Scanner;
+// O ternário também pode realizar uma operção lógica sem necessitar de uma declarar uma variável
+// Mas se torna mais complexo além de uma condicional simples, sendo necessário um aninhamento a cada condicional
+// extra adicionada
 
 public class UmACem {
     public static void main(String[] args) {
@@ -12,7 +15,11 @@ public class UmACem {
         int numero = scanner.nextInt();
 
         // SAÍDA DE DADOS
-        if(numero <= 100)
+        System.out.println(numero < 100
+                ? "O número " + numero + " está abaixo de 100 e está aprovado."
+                : (numero == 100
+                ? "O número " + numero + " está aprovado."
+                : "O número " + numero + " é maior que 100, e não foi aprovado."));
 
         // FECHAMENTO DE SCANNER
         scanner.close();
