221be04283f3973fa6862866a13ff769d99a3072
William Galvão <Liam_draven@outlook.com>
2025-01-28 05:10:23 -0300

    Create Iguais.java

---
+++ Conteudo/Java01BASICO/Aula04OperadoresRelacionais/Ex03Iguais/Iguais.java
@@ -0,0 +1,26 @@
+package Projeto201.Conteudo.Java01BASICO.Aula04OperadoresRelacionais.Ex03Iguais;
+
+import java.util.Scanner;
+
+public class Iguais {
+    public static void main(String[] args) {
+        // ABERTURA DE SCANNER
+        Scanner scanner = new Scanner(System.in);
+
+        // ENTRADA DE DADOS
+        System.out.println("Digite o primeiro número: ");
+        int num1 = scanner.nextInt();
+        System.out.println("Digite o segundo número: ");
+        int num2 = scanner.nextInt();
+
+        // SAÍDA DE DADOS
+        if(num1 == num2){
+            System.out.println("Os dois números são iguais");
+        }else{
+            System.out.println("Os dois números são diferentes");
+        }
+
+        // FECHAMENTO DE SCANNER
+        scanner.close();
+    }
+}
