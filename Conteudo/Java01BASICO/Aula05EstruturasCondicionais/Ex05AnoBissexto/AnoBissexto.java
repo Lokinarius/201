2ef3a0c74b53b318316ea0d27048399255a55c52
William Galvão <Liam_draven@outlook.com>
2025-01-29 14:56:25 -0300

    Create AnoBissexto.java

---
+++ Conteudo/Java01BASICO/Aula05EstruturasCondicionais/Ex05AnoBissexto/AnoBissexto.java
@@ -0,0 +1,24 @@
+package Projeto201.Conteudo.Java01BASICO.Aula05EstruturasCondicionais.Ex05AnoBissexto;
+
+import java.util.Scanner;
+
+public class AnoBissexto {
+    public static void main(String[] args) {
+        // ABERTURA DE SCANNER
+        Scanner scanner = new Scanner(System.in);
+
+        // ENTRADAS DE DADOS
+        System.out.println("Digite um ano: ");
+        int ano = scanner.nextInt();
+
+        // SAÍDA DE DADOS
+        if ((ano %4 == 0) && ((ano %100 != 0) || (ano %400 == 0))){
+            System.out.println("O ano "+ano+"  é um ano bissexto");
+        } else {
+            System.out.println("O ano "+ano+" não é um ano bissexto");
+        }
+
+        // FECHAMENTO DE SCANNER
+        scanner.close();
+    }
+}
