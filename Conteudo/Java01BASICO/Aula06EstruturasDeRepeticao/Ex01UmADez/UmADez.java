524dc2807e53d3b4d2c6703952f69005ab07e24f
William Galvão <Liam_draven@outlook.com>
2025-01-30 15:27:44 -0300

    Create UmADez.java

---
+++ Conteudo/Java01BASICO/Aula06EstruturasDeRepeticao/Ex01UmADez/UmADez.java
@@ -0,0 +1,11 @@
+package Projeto201.Conteudo.Java01BASICO.Aula06EstruturasDeRepeticao.Ex01UmADez;
+// A estrutura for repete um bloco de código um número determinado de vezes, geralmente composta por três partes:
+// inicialização, condição e atualização.
+public class UmADez {
+    public static void main(String[] args) {
+        System.out.println("Contando de 1 a 10");
+        for (int num = 0; num < 10 ; num++) {
+            System.out.println(num + 1);
+        }
+    }
+}
