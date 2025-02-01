ba4b77b1d3b769a1b922e741c75af0d077f2fd1e
William Galvão <Liam_draven@outlook.com>
2025-02-01 16:00:54 -0300

    Create VerificacaoArray.java

---
+++ Conteudo/Java01BASICO/Aula08Arrays/Ex05VerificacaoArray/VerificacaoArray.java
@@ -0,0 +1,17 @@
+package Projeto201.Conteudo.Java01BASICO.Aula08Arrays.Ex05VerificacaoArray;
+
+import java.util.Scanner;
+
+public class VerificacaoArray {
+    public static void main(String[] args) {
+        // ABERTURA DE SCANNER
+        Scanner scanner = new Scanner(System.in);
+
+        // ENTRADA DE DADOS
+        int[] n = {25,67,88,32,94,53};
+        System.out.println("Informe um número e verifique se ele é um dos ");
+
+        //
+        //
+    }
+}
