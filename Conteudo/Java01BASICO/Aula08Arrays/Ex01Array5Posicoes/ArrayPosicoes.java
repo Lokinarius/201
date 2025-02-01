c09533314497932336109ccf249d70ec10a86ff0
William Galvão <Liam_draven@outlook.com>
2025-02-01 16:00:31 -0300

    Create ArrayPosicoes.java

---
+++ Conteudo/Java01BASICO/Aula08Arrays/Ex01Array5Posicoes/ArrayPosicoes.java
@@ -0,0 +1,10 @@
+package Projeto201.Conteudo.Java01BASICO.Aula08Arrays.Ex01Array5Posicoes;
+
+public class ArrayPosicoes {
+    public static void main(String[] args) {
+        int[] n = {1,2,3,4,5};
+        for(int numeros : n) {
+            System.out.println(numeros);
+        }
+    }
+}
