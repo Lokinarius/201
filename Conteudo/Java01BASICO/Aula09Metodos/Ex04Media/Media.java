0a2a31c913b865d8f16735d97c348d8d453cfadc
William Galvão <Liam_draven@outlook.com>
2025-02-07 16:58:49 -0300

    Create Media.java

---
+++ Conteudo/Java01BASICO/Aula09Metodos/Ex04Media/Media.java
@@ -0,0 +1,38 @@
+package Projeto201.Conteudo.Java01BASICO.Aula09Metodos.Ex04Media;
+
+import java.util.Scanner;
+
+public class Media {
+    // Metodo soma
+    static int soma(int[] notas){
+        int soma = 0;
+        for(int nota: notas){
+            soma += nota;
+        }
+        return soma;
+    }
+
+    // Metodo media
+    static double media(int[] notas){
+        return soma(notas)/ notas.length;
+    }
+
+    public static void main(String[] args) {
+        //scanner
+        Scanner scanner = new Scanner(System.in);
+
+        //declaração do array
+        int[] notas = new int[4];
+
+        //entrada de dados para o loop
+        System.out.println("Digite suas 4 notas: ");
+        for(int i = 0; i < notas.length; i++){
+            System.out.println((i + 1)+"ª nota: ");
+            notas[i] = scanner.nextInt();
+        }
+        scanner.close();
+
+        System.out.println("\n A media das notas foi de "+media(notas));
+
+    }
+}
