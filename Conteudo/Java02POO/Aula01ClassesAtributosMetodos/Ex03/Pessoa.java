cd64a6845c52b08f9f74c696b3f21c21ff3e831b
William Galvão <Liam_draven@outlook.com>
2025-04-28 15:58:25 -0300

    Create Pessoa.java

---
+++ Conteudo/Java02POO/Aula01ClassesAtributosMetodos/Ex03/Pessoa.java
@@ -0,0 +1,13 @@
+package Projeto201.Conteudo.Java02POO.Aula01ClassesAtributosMetodos.Ex03;
+
+public class Pessoa {
+    // Atributos
+    public String nome;
+    public int idade;
+
+    // Metodos
+    public void seculo(){
+        System.out.println("Faltam "+(100 - idade)+" anos para "+nome+" atingir 100 anos");
+    }
+
+}
