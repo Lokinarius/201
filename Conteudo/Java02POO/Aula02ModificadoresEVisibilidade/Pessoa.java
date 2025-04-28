9fc588c72b8613abc11ac27609bc7a4aaff60ccf
William Galvão <Liam_draven@outlook.com>
2025-04-28 14:26:21 -0300

    Update Pessoa.java

--- Conteudo/Java02POO/Aula02ModificadoresEVisibilidade/Pessoa.java
+++ Conteudo/Java02POO/Aula02ModificadoresEVisibilidade/Pessoa.java
@@ -2,32 +2,10 @@ package Projeto201.Conteudo.Java02POO.Aula02ModificadoresEVisibilidade;
 
 public class Pessoa {
     // Atributos
-    private String nome;
-    private char sexo;
-    private int idade;
-    private double altura;
-
-    // CONSTRUTOR
-    public Pessoa(String nome, char sexo, int idade, double altura) {
-        this.nome = nome;
-        this.sexo = sexo;
-        this.idade = idade;
-        this.altura = altura;
-    }
-
-    // GETTERS E SETTERS
-
-    public String getNome() {return nome;}
-    public void setNome(String nome) {this.nome = nome;}
-
-    public char getSexo() {return sexo;}
-    public void setSexo(char sexo) {this.sexo = sexo;}
-
-    public int getIdade() {return idade;}
-    public void setIdade(int idade) {this.idade = idade;}
-
-    public double getAltura() {return altura;}
-    public void setAltura(double altura) {this.altura = altura;}
+    public String nome;
+    public char sexo;
+    public int idade;
+    public double altura;
 
     // METODOS
     public void exibirDados(){
