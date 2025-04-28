3429924a24d3ed3c1b381feee18c46d02a049048
William Galvão <Liam_draven@outlook.com>
2025-04-28 14:26:13 -0300

    Update PessoaTest.java

--- Conteudo/Java02POO/Aula02ModificadoresEVisibilidade/PessoaTest.java
+++ Conteudo/Java02POO/Aula02ModificadoresEVisibilidade/PessoaTest.java
@@ -13,20 +13,26 @@ public class PessoaTest {
         System.out.println("Quantas pessoas deseja cadastrar? ");
         int totPessoas = scanner.nextInt();
         scanner.nextLine();
-        for (int i = 1; i < totPessoas; i++) {
-            System.out.println("Cadastro da pessoa "+i+": ");
+        for (int i = 0; i < totPessoas; i++) {
+            System.out.println("Cadastro da pessoa "+(i+1)+": ");
+
+            Pessoa pessoa = new Pessoa();
 
             System.out.println("Nome: ");
             String nome = scanner.nextLine();
 
+            System.out.println("Sexo: ");
+            String sexoI = scanner.nextLine();
+            if(!sexoI.isEmpty()){
+                pessoa.sexo = sexoI.charAt(0);
+            }
+
             System.out.println("Idade: ");
             int idade = scanner.nextInt();
 
-            System.out.println("Sexo: ");
-            String sexo = scanner.nextLine();
-
             System.out.println("Altura: ");
             double altura = scanner.nextDouble();
+            scanner.nextLine();
         }
         // FECHAMENTO DO SCANNER
         scanner.close();
