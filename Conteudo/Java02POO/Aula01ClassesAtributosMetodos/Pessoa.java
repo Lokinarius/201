ff75630822cd4c68a13b189fd210b0b1c6fca3a1
William Galvão <Liam_draven@outlook.com>
2025-04-28 14:26:10 -0300

    Update Pessoa.java

--- Conteudo/Java02POO/Aula01ClassesAtributosMetodos/Pessoa.java
+++ Conteudo/Java02POO/Aula01ClassesAtributosMetodos/Pessoa.java
@@ -2,21 +2,8 @@ package Projeto201.Conteudo.Java02POO.Aula01ClassesAtributosMetodos;
 
 public class Pessoa {
     // ATRIBUTOS
-    private String nome;
-    private int idade;
-
-    // CONSTRUTOR
-    public Pessoa(String nome, int idade) {
-        this.nome = nome;
-        this.idade = idade;
-    }
-
-    // GETTERS E SETTERS
-    public String getNome() {return nome;}
-    public void setNome(String nome) {this.nome = nome;}
-
-    public int getIdade() {return idade;}
-    public void setIdade(int idade) {this.idade = idade;}
+    public String nome;
+    public int idade;
 
     // METODOS
     public void exibirDados(){
