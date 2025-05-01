6440029694ca3c08defc8acb46d1220334f9a3c3
William Galvão <Liam_draven@outlook.com>
2025-04-30 23:23:01 -0300

    Create Retangulo.java

---
+++ Conteudo/Java02POO/Aula01ClassesAtributosMetodos/Ex04Retangulo/Retangulo.java
@@ -0,0 +1,15 @@
+package Projeto201.Conteudo.Java02POO.Aula01ClassesAtributosMetodos.Ex04Retangulo;
+
+public class Retangulo {
+    // Atributos
+    public int base;
+    public int altura;
+
+    // Metodos
+    public void calculaArea(){
+        int area = base*altura;
+        System.out.println("O tamanho da área do retângulo é de: "+area);
+    }
+
+
+}
