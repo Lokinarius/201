ae8cb705f40e20ae3497555f4a3f9f6870cf2c99
William Galvão <Liam_draven@outlook.com>
2025-05-05 17:48:40 -0300

    Update Pessoa.java

--- Conteudo/Java02POO/Aula02ModificadoresEVisibilidade/Ex01Dados/Pessoa.java
+++ Conteudo/Java02POO/Aula02ModificadoresEVisibilidade/Ex01Dados/Pessoa.java
@@ -1,4 +1,23 @@
 package Projeto201.Conteudo.Java02POO.Aula02ModificadoresEVisibilidade.Ex01Dados;
 
 public class Pessoa {
+    // ATRIBUTOS
+    private String nome;
+    private char sexo;
+    private int idade;
+    private double altura;
+
+    // Setters
+    public void setNome(String nome) {this.nome = nome;}
+    public void setSexo(char sexo) {this.sexo = sexo;}
+    public void setIdade(int idade) {this.idade = idade;}
+    public void setAltura(double altura) {this.altura = altura;}
+
+    // METODOS
+    public void exibirDados(){
+        System.out.println("Nome: "+nome);
+        System.out.println("Sexo: "+sexo);
+        System.out.println("Idade: "+idade);
+        System.out.println("altura: "+altura);
+    }
 }
