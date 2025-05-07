f57eae1db218bff89f1deccd5f3ae0f8c7bc5545
William Galvão <Liam_draven@outlook.com>
2025-05-07 14:43:39 -0300

    Update PessoaTest.java

--- Conteudo/Java02POO/Aula02ModificadoresEVisibilidade/Ex02DadosAlteraveis/PessoaTest.java
+++ Conteudo/Java02POO/Aula02ModificadoresEVisibilidade/Ex02DadosAlteraveis/PessoaTest.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class PessoaTest {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner();
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Quantas pessoas deseja cadastrar? ");
         int totPessoas = scanner.nextInt();
@@ -53,9 +53,57 @@ public class PessoaTest {
         // MENU INTERATIVO
         int opcao;
         do{
+            // Menu
+            System.out.println("\n==== MENU ====");
+            System.out.println("[1] - Exibir todas pessoas");
+            System.out.println("[2] - Alterar idade");
+            System.out.println("[3] - Alterar nome");
+            System.out.println("[0] - Sair");
+            opcao = scanner.nextInt();
+            scanner.nextLine();
 
-        }while(opcao != o);
+            // Switch
+            switch (opcao){
+                case 1:
+                    System.out.println("=== Lista de Pessoas Cadastradas ===");
+                    for (int i = 0; i < pessoas.length; i++) {
+                        System.out.println("Pessoa "+(i+1)+":");
+                        pessoas[i].exibirDados();
+                        System.out.println("-------------------");
+                    }
+                    break;
+                case 2:
+                    System.out.println("Digite a posição da pessoa: ");
+                    int posIdade = scanner.nextInt() - 1;
+                    if (posIdade >= 0 && posIdade < totPessoas){
+                        System.out.println("Nova idade: ");
+                        int novaIdade = scanner.nextInt();
+                        pessoas[posIdade].alterarIdade(novaIdade);
+                    }else{
+                        System.out.println("Posição invalida.");
+                    }
+                    break;
+                case 3:
+                    System.out.println("Digite a posição da pessoa: ");
+                    int posNome = scanner.nextInt() - 1;
+                    scanner.nextLine();
+                    if(posNome >= 0 && posNome < totPessoas){
+                        System.out.println("Novo nome: ");
+                        String novoNome = scanner.nextLine();
+                        pessoas[posNome].alterarNome(novoNome);
+                    }else{
+                        System.out.println("Posição invalida.");
+                    }
+                    break;
+                case 0:
+                    System.out.println("Encerrando o programa.");
+                    break;
+                default:
+                    System.out.println("Opção invalida.");
+            }
+        }while(opcao != 0);
 
+        // Fechamento de scanner
         scanner.close();
     }
 }
