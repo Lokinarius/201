067203969e08f0ef43f1ebfaad3f35b250ac27b1
William Galvão <Liam_draven@outlook.com>
2025-05-08 18:03:11 -0300

    Update ContaBancaria.java

--- Conteudo/Java02POO/Aula02ModificadoresEVisibilidade/Ex03ContaBancaria/ContaBancaria.java
+++ Conteudo/Java02POO/Aula02ModificadoresEVisibilidade/Ex03ContaBancaria/ContaBancaria.java
@@ -1,4 +1,25 @@
 package Projeto201.Conteudo.Java02POO.Aula02ModificadoresEVisibilidade.Ex03ContaBancaria;
 
 public class ContaBancaria {
+    // ATRIBUTOS
+    private String usuario;
+    private int conta;
+    private double saldo;
+
+    // GETTERS E SETTERS
+    public String getUsuario() {return usuario;}
+    public void setUsuario(String usuario) {this.usuario = usuario;}
+
+    public int getConta() {return conta;}
+    public void setConta(int conta) {this.conta = conta;}
+
+    public double getSaldo() {return saldo;}
+    public void setSaldo(double saldo) {this.saldo = saldo;}
+
+    // METODOS
+    public void exibirSaldo(){
+        System.out.println("Usuário: "+usuario);
+        System.out.println("Conta: "+ conta);
+        System.out.println("Saldo: "+saldo);
+    }
 }
