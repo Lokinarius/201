85f97a54bfa6943ca069541b89945128d1b4f182
William Galvão <Liam_draven@outlook.com>
2025-05-11 15:48:52 -0300

    Update Livro.java

--- Conteudo/Java02POO/Aula02ModificadoresEVisibilidade/Ex05Livro/Livro.java
+++ Conteudo/Java02POO/Aula02ModificadoresEVisibilidade/Ex05Livro/Livro.java
@@ -1,4 +1,25 @@
 package Projeto201.Conteudo.Java02POO.Aula02ModificadoresEVisibilidade.Ex05Livro;
 
 public class Livro {
+    // ATRIBUTOS
+    private String titulo;
+    private String autor;
+    private int numeroPaginas;
+
+    //CONSTRUTOR
+
+
+    public Livro(String titulo, String autor, int numeroPaginas) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    // METODOS
+    public void exibirDados(){
+        System.out.println("Título: "+titulo);
+        System.out.println("Autor: "+autor);
+        System.out.println("Numero de páginas: "+numeroPaginas);
+    }
+
 }
