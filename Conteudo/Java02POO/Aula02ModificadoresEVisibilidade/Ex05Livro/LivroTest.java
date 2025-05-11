db4249fa3158c38bac2fbef4cdd7bd1e502dd77e
William Galvão <Liam_draven@outlook.com>
2025-05-11 15:48:54 -0300

    Update LivroTest.java

--- Conteudo/Java02POO/Aula02ModificadoresEVisibilidade/Ex05Livro/LivroTest.java
+++ Conteudo/Java02POO/Aula02ModificadoresEVisibilidade/Ex05Livro/LivroTest.java
@@ -1,4 +1,13 @@
 package Projeto201.Conteudo.Java02POO.Aula02ModificadoresEVisibilidade.Ex05Livro;
 
 public class LivroTest {
+    public static void main(String[] args) {
+        Livro livro1 = new Livro("1984","Georde Orwell",328);
+        Livro livro2 = new Livro("Harry Potter e a Pedra filosofal","J.K.Rowlling",223);
+        Livro livro3 = new Livro("O Pequeno Principe","Antoine de Saint-Exupéry",96);
+
+        livro1.exibirDados();
+        livro2.exibirDados();
+        livro3.exibirDados();
+    }
 }
