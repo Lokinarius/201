0e02a39a116fc8baeee6d5db28f5b92bfc74aa67
William Galvão <Liam_draven@outlook.com>
2025-05-17 16:22:11 -0300

    Update PessoaTest.java

--- Conteudo/Java02POO/Aula03Construtores/Ex01Pessoa/PessoaTest.java
+++ Conteudo/Java02POO/Aula03Construtores/Ex01Pessoa/PessoaTest.java
@@ -4,9 +4,18 @@ import java.util.Scanner;
 
 public class PessoaTest {
     public static void main(String[] args) {
-        // ABERTURA DE SCANNER
-        Scanner scanner = new Scanner(System.in);
-        // FECHAMENTO DE SCANNER
-        scanner.close();
+
+        Pessoa[] pessoas = new Pessoa[4];
+        pessoas[1] = new Pessoa("William",29);
+        pessoas[2] = new Pessoa("Marcos",32);
+        pessoas[3] = new Pessoa("Tainá",25);
+        pessoas[4] = new Pessoa("Hugo",34);
+        
+        // Saída de dados
+        System.out.println("=== Pessoas Cadastradas ===");
+        for (Pessoa p : pessoas){
+            p.exibirDados();
+            System.out.println("------------------------");
+        }
     }
 }
