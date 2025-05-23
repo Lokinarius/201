aa351cbca2374cf5e9ca91fce45107e85b23ec66
William Galvão <Liam_draven@outlook.com>
2025-05-23 14:42:33 -0300

    Update FuncionarioTest.java

--- Conteudo/Java02POO/Aula04GettersESetters/Ex05Funcionario/FuncionarioTest.java
+++ Conteudo/Java02POO/Aula04GettersESetters/Ex05Funcionario/FuncionarioTest.java
@@ -1,12 +1,17 @@
 package Projeto201.Conteudo.Java02POO.Aula04GettersESetters.Ex05Funcionario;
 
 public class FuncionarioTest {
-    // ABERTURA DE SCANNER
+    public static void main(String[] args) {
+        Funcionario[] funcionarios = new Funcionario[5];
+        funcionarios[0] = new Funcionario("Carlinhos","Veterinário",2050);
+        funcionarios[1] = new Funcionario("Leandro","Advogado",4000);
+        funcionarios[2] = new Funcionario("Gabriela","Psicóloga",3500);
+        funcionarios[3] = new Funcionario("Sandra","Professora",2800);
+        funcionarios[4] = new Funcionario("Wesley","Designer",3000);
 
-    // ENTRADA DE DADOS
-
-    // SAÍDA DE DADOS
-
-    // FECHAMENTO DE SCANNER
-
+        System.out.println("============== FUNCIONARIOS ==============");
+        for(Funcionario f : funcionarios){
+            f.ExibirDados();
+        }
+    }
 }
