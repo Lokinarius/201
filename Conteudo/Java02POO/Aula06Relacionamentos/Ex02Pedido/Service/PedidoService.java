c856ffc57d16b3d288333d4f9f47c9341b23ee9f
William Galvão <Liam_draven@outlook.com>
2025-06-06 19:28:43 -0300

    Create PedidoService.java

---
+++ Conteudo/Java02POO/Aula06Relacionamentos/Ex02Pedido/Service/PedidoService.java
@@ -0,0 +1,19 @@
+package Projeto201.Conteudo.Java02POO.Aula06Relacionamentos.Ex02Pedido.Service;
+
+import Projeto201.Conteudo.Java02POO.Aula06Relacionamentos.Ex02Pedido.Domain.Pedido;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PedidoService {
+    private List<Pedido> pedidos = new ArrayList<>();
+
+    // ADICIONAR PEDIDO
+
+    // REMOVER PEDIDO
+
+    // BUSCAR PEDIDO
+
+    // LISTAR PEDIDOS
+
+}
