08253434a04c2b411ba77f932d5307c784193122
William Galvão <Liam_draven@outlook.com>
2025-06-07 14:28:06 -0300

    Update PedidoService.java

--- Conteudo/Java02POO/Aula06Relacionamentos/Ex02Pedido/Service/PedidoService.java
+++ Conteudo/Java02POO/Aula06Relacionamentos/Ex02Pedido/Service/PedidoService.java
@@ -9,11 +9,35 @@ public class PedidoService {
     private List<Pedido> pedidos = new ArrayList<>();
 
     // ADICIONAR PEDIDO
+    public void adicionar(Pedido pedido){
+        pedidos.add(pedido);
+        System.out.println("Pedido adicionado com sucesso!");
+    }
 
     // REMOVER PEDIDO
+    public void removerPedido(int indice){
+        if(indice >= 0 && indice < pedidos.size()){
+            pedidos.remove(indice);
+            System.out.println("Pedido removido com sucesso!");
+        }else{
+            System.out.println("Número do pedido não encontrado");
+        }
+    }
 
     // BUSCAR PEDIDO
+    public Pedido buscarPedido(int indice){
+        if(indice >= 0 && indice < pedidos.size()){
+            return pedidos.get(indice);
+        }
+        return null;
+    }
 
     // LISTAR PEDIDOS
+    public void listarPedidos(){
+        for (int i = 0; i < pedidos.size(); i++) {
+            System.out.println("Pedido " +(i+1)+": ");
+            pedidos.get(i).exibirDados();
+        }
+    }
 
 }
