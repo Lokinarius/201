c8815bb501724284a0c3f2a43bbf10d20e255e18
William Galvão <Liam_draven@outlook.com>
2025-06-09 21:35:01 -0300

    Update PedidoService.java

--- Conteudo/Java02POO/Aula06Relacionamentos/Ex02Pedido/Service/PedidoService.java
+++ Conteudo/Java02POO/Aula06Relacionamentos/Ex02Pedido/Service/PedidoService.java
@@ -4,18 +4,33 @@ import Projeto201.Conteudo.Java02POO.Aula06Relacionamentos.Ex02Pedido.Domain.Ped
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class PedidoService {
     private List<Pedido> pedidos = new ArrayList<>();
 
     // ADICIONAR PEDIDO
-    public void adicionar(Pedido pedido){
-        pedidos.add(pedido);
-        System.out.println("Pedido adicionado com sucesso!");
+    public void adicionar(Scanner scanner){
+        System.out.println("Digite o nome do cliente: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite o Produto: ");
+        String produto = scanner.nextLine();
+
+        System.out.println("Digite a quantidade: ");
+        int quantidade = scanner.nextInt();
+        scanner.nextLine();
+
+        Pedido novoPedido = new Pedido(nome, produto, quantidade);
+        pedidos.add(novoPedido);
+
+        System.out.println("Pedido adicionado!");
+
     }
 
     // REMOVER PEDIDO
     public void removerPedido(int indice){
+        System.out.println("Digite o número");
         if(indice >= 0 && indice < pedidos.size()){
             pedidos.remove(indice);
             System.out.println("Pedido removido com sucesso!");
