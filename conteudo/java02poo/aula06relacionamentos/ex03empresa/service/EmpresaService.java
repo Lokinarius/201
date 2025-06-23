a11c1cef07358686e4e3d61c21de63020630b58e
William Galvão <Liam_draven@outlook.com>
2025-06-23 16:19:59 -0300

    Create EmpresaService.java

---
+++ conteudo/java02poo/aula06relacionamentos/ex03empresa/service/EmpresaService.java
@@ -0,0 +1,20 @@
+package projeto201.conteudo.java02poo.aula06relacionamentos.ex03empresa.service;
+
+import projeto201.conteudo.java02poo.aula06relacionamentos.ex03empresa.domain.Funcionario;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmpresaService {
+    private List<Funcionario> funcionarios = new ArrayList<>();
+    // LISTAR FUNCIONARIOS
+
+    // BUSCAR FUNCIONARIO
+
+    // CONTRATAR FUNCIONARIO
+
+    // CONCEDER AUMENTO
+
+    // DEMITIR FUNCIONARIO
+
+}
