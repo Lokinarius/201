3048f18dd90c637e520fa648c03f74cc84e74aa0
William Galvão <Liam_draven@outlook.com>
2025-06-27 17:44:49 -0300

    Create ConcessionariaService.java

---
+++ conteudo/java02poo/aula06relacionamentos/ex04carro/service/ConcessionariaService.java
@@ -0,0 +1,17 @@
+package projeto201.conteudo.java02poo.aula06relacionamentos.ex04carro.service;
+
+public class ConcessionariaService {
+
+    // LISTAR CARROS
+
+    // LISTAR PROPRIETARIOS
+
+    // BUSCAR CARROS
+
+    // BUSCAR PROPRIETARIOS
+
+    // VENDA DE CARRO
+
+    // COMPRA DE CARRO
+
+}
