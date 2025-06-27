7e5603519ce91490b4651bc809a87c6f0f4c756b
William Galvão <Liam_draven@outlook.com>
2025-06-27 17:56:58 -0300

    Update ConcessionariaService.java

--- conteudo/java02poo/aula06relacionamentos/ex04carro/service/ConcessionariaService.java
+++ conteudo/java02poo/aula06relacionamentos/ex04carro/service/ConcessionariaService.java
@@ -1,17 +1,34 @@
 package projeto201.conteudo.java02poo.aula06relacionamentos.ex04carro.service;
 
 public class ConcessionariaService {
-
     // LISTAR CARROS
+    public void listarCarros() {
+
+    }
 
     // LISTAR PROPRIETARIOS
+    public void listarProprietarios() {
+
+    }
 
     // BUSCAR CARROS
+    public void buscarCarro() {
+
+    }
 
     // BUSCAR PROPRIETARIOS
+    public void buscarProprietario() {
+
+    }
 
     // VENDA DE CARRO
+    public void vendaCarro() {
+
+    }
 
     // COMPRA DE CARRO
+    public void compraCarro() {
+
+    }
 
 }
