5eea9a020c55a8002aa1fc989077c6cf2444c386
William Galvão <Liam_draven@outlook.com>
2025-06-28 15:17:12 -0300

    Update CriarProprietario.java

--- conteudo/java02poo/aula06relacionamentos/ex04carro/domain/CriarProprietario.java
+++ conteudo/java02poo/aula06relacionamentos/ex04carro/domain/CriarProprietario.java
@@ -7,13 +7,13 @@ public class CriarProprietario {
     public static List<Proprietario> gerarProprietarios(){
         List<Proprietario> proprietarios = new ArrayList<>();
 
-        proprietarios.add(new Proprietario("","",new ArrayList<>()));
-        proprietarios.add(new Proprietario("","",new ArrayList<>()));
-        proprietarios.add(new Proprietario("","",new ArrayList<>()));
-        proprietarios.add(new Proprietario("","",new ArrayList<>()));
-        proprietarios.add(new Proprietario("","",new ArrayList<>()));
-        proprietarios.add(new Proprietario("","",new ArrayList<>()));
-        proprietarios.add(new Proprietario("","",new ArrayList<>()));
+        proprietarios.add(new Proprietario("João Silva", "111.111.111-11", new ArrayList<>()));
+        proprietarios.add(new Proprietario("Maria Oliveira", "222.222.222-22", new ArrayList<>()));
+        proprietarios.add(new Proprietario("Carlos Souza", "333.333.333-33", new ArrayList<>()));
+        proprietarios.add(new Proprietario("Ana Paula", "444.444.444-44", new ArrayList<>()));
+        proprietarios.add(new Proprietario("Fernanda Costa", "555.555.555-55", new ArrayList<>()));
+        proprietarios.add(new Proprietario("Pedro Henrique", "666.666.666-66", new ArrayList<>()));
+        proprietarios.add(new Proprietario("Lucas Lima", "777.777.777-77", new ArrayList<>()));
 
         return proprietarios;
     }
