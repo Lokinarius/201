2a8b403f56b1686b98deee6fbab79c8866f2f34d
William Galvão <Liam_draven@outlook.com>
2025-06-30 16:29:31 -0300

    Update CursoService.java

--- conteudo/java02poo/aula06relacionamentos/ex05curso/service/CursoService.java
+++ conteudo/java02poo/aula06relacionamentos/ex05curso/service/CursoService.java
@@ -1,4 +1,11 @@
 package projeto201.conteudo.java02poo.aula06relacionamentos.ex05curso.service;
 
 public class CursoService {
+    // LISTAR ALUNOS
+
+    // MATRICULADOS NA MATERIA
+
+    // MATRICULAR ALUNO
+
+    // REMOVER ALUNO
 }
