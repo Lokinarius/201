705673b0f53efa8bfad82fb814be7f2d418aefa4
William Galvão <Liam_draven@outlook.com>
2025-07-01 15:13:33 -0300

    Create EscolaService.java

---
+++ conteudo/java02poo/aula06relacionamentos/ex05curso/service/EscolaService.java
@@ -0,0 +1,28 @@
+package projeto201.conteudo.java02poo.aula06relacionamentos.ex05curso.service;
+
+public class EscolaService {
+
+    // LISTAR ALUNOS
+    public static void listarAlunos() {
+    }
+
+    // MATRICULADOS NO CURSO
+    public static void matriculadosCurso() {
+    }
+
+    // MATRICULAR ALUNO
+    public static void matricularAluno() {
+    }
+
+    // TROCAR DE CURSO
+    public static void removerAluno() {
+    }
+
+    // REMOVER ALUNO
+    public static void trocarDecurso() {
+    }
+
+
+
+
+}
