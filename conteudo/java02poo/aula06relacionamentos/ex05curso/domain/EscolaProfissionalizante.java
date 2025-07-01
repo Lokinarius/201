b04928ff267f9cb5ca43de6cdd6492050f7d86fb
William Galvão <Liam_draven@outlook.com>
2025-07-01 15:13:37 -0300

    Create EscolaProfissionalizante.java

---
+++ conteudo/java02poo/aula06relacionamentos/ex05curso/domain/EscolaProfissionalizante.java
@@ -0,0 +1,17 @@
+package projeto201.conteudo.java02poo.aula06relacionamentos.ex05curso.domain;
+
+import java.util.List;
+
+public class EscolaProfissionalizante {
+    // ATRIBUTOS
+    private String nome;
+    private String sede;
+    private List
+
+    // CONSTRUTOR
+
+    // GETTER AND SETTERS
+
+    // METODOS
+
+}
