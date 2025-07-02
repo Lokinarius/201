3c1c162493374eec87033af0b044618b6a08ca5f
William Galvão <Liam_draven@outlook.com>
2025-07-02 15:00:49 -0300

    Create CriarSede.java

---
+++ conteudo/java02poo/aula06relacionamentos/ex05curso/domain/CriarSede.java
@@ -0,0 +1,18 @@
+package projeto201.conteudo.java02poo.aula06relacionamentos.ex05curso.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CriarSede {
+    public static List<Sede> gerarSedes(){
+// Cria sedes com listas de alunos inicialmente vazias
+        List<Sede> sedes = new ArrayList<>();
+        sedes.add(new Sede("Campus Central", new ArrayList<>()));
+        sedes.add(new Sede("Unidade Zona Norte", new ArrayList<>()));
+        sedes.add(new Sede("Unidade Zona Sul", new ArrayList<>()));
+
+        CriarAluno.gerarAlunos(sedes);
+
+        return sedes;
+    }
+}
