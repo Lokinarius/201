075afc3c9b541d1e84467cbc9074526cf2526d76
William Galvão <Liam_draven@outlook.com>
2025-07-03 13:39:39 -0300

    Update EscolaService.java

--- conteudo/java02poo/aula06relacionamentos/ex05curso/service/EscolaService.java
+++ conteudo/java02poo/aula06relacionamentos/ex05curso/service/EscolaService.java
@@ -1,28 +1,107 @@
 package projeto201.conteudo.java02poo.aula06relacionamentos.ex05curso.service;
 
+import projeto201.conteudo.java02poo.aula06relacionamentos.ex05curso.domain.Aluno;
+import projeto201.conteudo.java02poo.aula06relacionamentos.ex05curso.domain.EscolaProfissionalizante;
+import projeto201.conteudo.java02poo.aula06relacionamentos.ex05curso.domain.Sede;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class EscolaService {
+    private static EscolaProfissionalizante escola;
+    private static Scanner scanner = new Scanner(System.in);
 
     // LISTAR ALUNOS
     public static void listarAlunos() {
+        for(Sede sede : escola.getSedes()){
+            for(Aluno aluno: sede.getAlunos()){
+                aluno.exibirDados();
+            }
+        }
     }
 
-    // MATRICULADOS NO CURSO
-    public static void matriculadosCurso() {
+    // LISTAR ALUNOS POR SEDE
+    public static void matriculadosCurso(){
+        escola.exibirDados();
     }
+
+
+    // BUSCAR ALUNO
+    public static Aluno buscarPorNomeAluno(String nome){
+        for (Sede sede: escola.getSedes()){
+            for(Aluno aluno:sede.getAlunos()){
+                if(aluno.getNome().equalsIgnoreCase(nome)){
+                    return aluno;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static Aluno buscarAluno(){
+        System.out.println("Digite o nome do Aluno: ");
+        String nome = scanner.nextLine();
+        Aluno a = buscarPorNomeAluno(nome);
+        if(a == null){
+            System.out.println("Aluno não encontrado");
+        }else{
+            a.exibirDados();
+        }
+        return a;
+    }
+
 
     // MATRICULAR ALUNO
     public static void matricularAluno() {
-    }
+        System.out.println("Nome: ");
+        String nome = scanner.nextLine();
+        System.out.println("Matrícula: ");
+        int matricula = scanner.nextInt();
+        System.out.println("Curso: ");
+        String curso = scanner.nextLine();
+        System.out.println("Período: ");
+        int periodo = scanner.nextInt();
+        List<Sede> sedes = escola.getSedes();
 
-    // TROCAR DE CURSO
-    public static void removerAluno() {
+        System.out.println("Escolha a sede: ");
+        for (int i = 0; i < sedes.size(); i++) {
+            System.out.println("["+(i+1)+"]"+sedes.get(i).getNome());
+        }
+
+        int opcao = scanner.nextInt() - 1;
+        if(opcao >= 0 && opcao < sedes.size()){
+            Sede sedeEscolhida = sedes.get(opcao);
+            Aluno novoAluno = new Aluno(nome,matricula,curso,periodo,sedeEscolhida);
+            sedeEscolhida.getAlunos().add(novoAluno);
+            System.out.println("Aluno matriculado!");
+        }else{
+            System.out.println("opção invalida.");
+        }
     }
 
     // REMOVER ALUNO
-    public static void trocarDecurso() {
+    public static void removerAluno() {
+        Aluno aluno = buscarAluno();
+        if (aluno != null){
+            Sede sede = aluno.getSede();
+            if(sede.getAlunos().remove(aluno)){
+                System.out.println("Aluno removido");
+            }else{
+                System.out.println("Aluno não encontrado");
+            }
+        }
     }
 
+    // TROCAR DE CURSO
+    public static void trocarDecurso() {
+        Aluno aluno = buscarAluno();
+        if(aluno != null){
+            System.out.println("Digite o novo curso: ");
+            String novoCurso = scanner.nextLine();
+            aluno.setCurso(novoCurso);
+            System.out.println("Curso atualizado");
+        }
 
-
-
+    }
 }
