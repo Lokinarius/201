4dbcfc621a1f34b7960f804248f9b7871e9f93f6
William Galvão <Liam_draven@outlook.com>
2025-07-09 15:46:06 -0300

    correções

--- conteudo/java02poo/aula06relacionamentos/ex01endereco/domain/Pessoa.java
+++ conteudo/java02poo/aula06relacionamentos/ex01endereco/domain/Pessoa.java
@@ -3,21 +3,21 @@ package projeto201.conteudo.java02poo.aula06relacionamentos.ex01endereco.domain;
 public class Pessoa {
     // ATRIBUTOS
     private String nome;
-    private int cpf;
+    private long cpf;
     private Endereco endereco;
 
     // GETTERS E SETTERS
     public String getNome() {return nome;}
     public void setNome(String nome) {this.nome = nome;}
 
-    public int getCpf() {return cpf;}
-    public void setCpf(int cpf) {this.cpf = cpf;}
+    public long getCpf() {return cpf;}
+    public void setCpf(long cpf) {this.cpf = cpf;}
 
     public Endereco getEndereco() {return endereco;}
     public void setEndereco(Endereco endereco) {this.endereco = endereco;}
 
     // CONSTRUTOR
-    public Pessoa(String nome, int cpf, Endereco endereco) {
+    public Pessoa(String nome, long cpf, Endereco endereco) {
         setNome(nome);
         setCpf(cpf);
         setEndereco(endereco);
--- conteudo/java02poo/aula06relacionamentos/ex01endereco/test/EnderecoTest.java
+++ conteudo/java02poo/aula06relacionamentos/ex01endereco/test/EnderecoTest.java
@@ -11,13 +11,14 @@ public class EnderecoTest {
     public static void main(String[] args) {
         // DADOS
         List<Pessoa> pessoas = new ArrayList<>();
-        pessoas.add(new Pessoa("Alice", 123456789, new Endereco("Sobral", "Centro", "Rua das Flores", 101)));
-        pessoas.add(new Pessoa("Bruno", 987654321, new Endereco("Fortaleza", "Meireles", "Av. Beira Mar", 200)));
-        pessoas.add(new Pessoa("Carla", 111222333, new Endereco("Juazeiro", "Salesianos", "Rua São Pedro", 55)));
-        pessoas.add(new Pessoa("Daniel", 444555666, new Endereco("Crato", "Pimenta", "Rua das Palmeiras", 78)));
-        pessoas.add(new Pessoa("Eduarda", 777888999, new Endereco("Tianguá", "Centro", "Rua Projetada", 32)));
-        pessoas.add(new Pessoa("Felipe", 121314151, new Endereco("Sobral", "Dom Expedito", "Rua Nova", 8)));
-        pessoas.add(new Pessoa("Gabriela", 161718192, new Endereco("Fortaleza", "Aldeota", "Av. Santos Dumont", 999)));
+        pessoas.add(new Pessoa("Alice", 12345678967L, new Endereco("Sobral", "Centro", "Rua das Flores", 101)));
+        pessoas.add(new Pessoa("Bruno", 98765432154L, new Endereco("Fortaleza", "Meireles", "Av. Beira Mar", 200)));
+        pessoas.add(new Pessoa("Carla", 11122233322L, new Endereco("Juazeiro", "Salesianos", "Rua São Pedro", 55)));
+        pessoas.add(new Pessoa("Daniel", 44455566614L, new Endereco("Crato", "Pimenta", "Rua das Palmeiras", 78)));
+        pessoas.add(new Pessoa("Eduarda", 77788899916L, new Endereco("Tianguá", "Centro", "Rua Projetada", 32)));
+        pessoas.add(new Pessoa("Felipe", 12131415178L, new Endereco("Sobral", "Dom Expedito", "Rua Nova", 8)));
+        pessoas.add(new Pessoa("Gabriela", 16171819290L, new Endereco("Fortaleza", "Aldeota", "Av. Santos Dumont",
+                999)));
 
         // ABERTURA DE SCANNER
         Scanner scanner = new Scanner(System.in);
@@ -40,7 +41,7 @@ public class EnderecoTest {
             switch (opcao){
                 case 1:
                     System.out.println("CPF da pessoa: ");
-                    int cpfPessoa = scanner.nextInt();
+                    long cpfPessoa = scanner.nextLong();
                     scanner.nextLine();
 
                     Pessoa pessoaSelecionada = null;
@@ -79,28 +80,28 @@ public class EnderecoTest {
                                     String novaRua = scanner.nextLine();
 
                                     System.out.println("Digite o novo número: ");
-                                    int novoNumero = scanner.nextInt();
+                                    long novoNumero = scanner.nextLong();
                                     scanner.nextLine();
 
                                     pessoaSelecionada.getEndereco().setCidade(novaCidade);
                                     pessoaSelecionada.getEndereco().setBairro(novoBairro);
                                     pessoaSelecionada.getEndereco().setRua(novaRua);
-                                    pessoaSelecionada.getEndereco().setNumero(novoNumero);
+                                    pessoaSelecionada.getEndereco().setNumero((int) novoNumero);
 
-                                    System.out.println(" Endereço atualizado !");
+                                    System.out.println(" Endereço atualizado !\n");
                                     break;
 
                                 case 0:
-                                    System.out.println("Retornando ao menu principal...");
+                                    System.out.println("Retornando ao menu principal...\n");
                                     break;
 
                                 default:
-                                    System.out.println("opção inválida");
+                                    System.out.println("opção inválida\n");
 
                             }
                         }while(operacao != 0);
                     }else {
-                        System.out.println("Pessoa não encontrada ");
+                        System.out.println("Pessoa não encontrada \n");
                     }
                     break;
 
@@ -109,31 +110,42 @@ public class EnderecoTest {
                     String novoNome = scanner.nextLine();
 
                     System.out.println("Digite o CPF da nova pessoa: ");
-                    int novoCpf = scanner.nextInt();
+                    long novoCpf = scanner.nextLong();
                     scanner.nextLine();
+
+                    // Validação para cpf duplicado
+                    boolean cpfExiste = false;
+                    for(Pessoa p : pessoas){
+                        if(p.getCpf() == novoCpf){
+                            cpfExiste = true;
+                            break;
+                        }
+                    }
+                    if(cpfExiste){
+                        System.out.println("Já existe uma pessoa cadastrada com este CPF!");
+                        break;
+                    }
 
                     System.out.println("Digite a Cidade onde ela vive: ");
-                    String novaCidade = scanner.nextLine();
+                    String cidade = scanner.nextLine();
 
                     System.out.println("Digite o Bairro onde ela mora: ");
-                    String novoBairro = scanner.nextLine();
+                    String bairro = scanner.nextLine();
 
                     System.out.println("Digite a rua onde ela mora: ");
-                    String novaRua = scanner.nextLine();
+                    String rua = scanner.nextLine();
 
                     System.out.println("Digite o número do endereço: ");
-                    int novoNumero = scanner.nextInt();
+                    int numero = scanner.nextInt();
                     scanner.nextLine();
 
-                    Pessoa novaPessoa = new Pessoa(novoNome,novoCpf,new Endereco(novaCidade,novoBairro,novaRua,
-                            novoNumero));
-                    pessoas.add(novaPessoa);
-                    System.out.println(novoNome+" Adicionada a lista");
+                    pessoas.add(new Pessoa(novoNome, novoCpf, new Endereco(cidade, bairro, rua, numero)));
+                    System.out.println(novoNome+" adicionada com sucesso!\n");
                     break;
 
                 case 3:
                     System.out.println("Digite o CPF da pessoa a ser removida");
-                    int cpfRemover = scanner.nextInt();
+                    long cpfRemover = scanner.nextLong();
                     scanner.nextLine();
 
                     Pessoa pessoaRemover = null;
@@ -146,9 +158,9 @@ public class EnderecoTest {
 
                     if(pessoaRemover != null){
                         pessoas.remove(pessoaRemover);
-                        System.out.println("Pessoa removida");
+                        System.out.println("Pessoa removida\n");
                     }else{
-                        System.out.println("Pessoa não encontrada");
+                        System.out.println("Pessoa não encontrada\n");
                     }
                     break;
 
