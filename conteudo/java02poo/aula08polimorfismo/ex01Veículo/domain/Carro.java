d61413c25661df7200de5d641a07ee83fe27b010
William Galvão <Liam_draven@outlook.com>
2025-08-19 16:45:14 -0300

    Create Carro.java

---
+++ conteudo/java02poo/aula08polimorfismo/ex01Veículo/domain/Carro.java
@@ -0,0 +1,25 @@
+package projeto201.conteudo.java02poo.aula08polimorfismo.ex01Veículo.domain;
+
+public class Carro extends Veiculo {
+    private int portas;
+
+    public Carro(String marca, String modelo, int ano, int roda, double tanque, double consumo, int portas) {
+        super(marca, modelo, ano, roda, tanque, consumo);
+        this.portas = portas;
+    }
+
+    @Override
+    public void mover() {
+        System.out.println("O carro está sendo dirigido nas ruas");
+    }
+
+    @Override
+    public void abastecer() {
+        System.out.println("gasolina/álcool/diesel");
+    }
+
+    @Override
+    public double calcularAutonomia() {
+        return 0;
+    }
+}
