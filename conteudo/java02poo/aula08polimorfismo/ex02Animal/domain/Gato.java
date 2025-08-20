44ddf0b6c99da5d335b06c96347a464556de53e8
William Galvão <Liam_draven@outlook.com>
2025-08-20 16:47:06 -0300

    Create Gato.java

---
+++ conteudo/java02poo/aula08polimorfismo/ex02Animal/domain/Gato.java
@@ -0,0 +1,35 @@
+package projeto201.conteudo.java02poo.aula08polimorfismo.ex02Animal.domain;
+
+public class Gato extends Animal{
+    // atributos
+    private String pelagem;
+
+    //construtor
+
+
+    public Gato(String nome, int idade, double peso, String pelagem) {
+        super(nome, idade, peso);
+        this.pelagem = pelagem;
+    }
+
+    // metodos
+    @Override
+    public void emitirSom() {
+        System.out.println("miado");
+    }
+
+    @Override
+    public void mover() {
+        System.out.println("anda silenciosamente");
+    }
+
+    @Override
+    public void alimentar() {
+        System.out.println("ração/peixe");
+    }
+
+    @Override
+    protected String detalhes() {
+        return "Pelagem: "+pelagem;
+    }
+}
