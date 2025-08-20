1303785b533d69b4bf30ca4aaff0a08173208416
William Galvão <Liam_draven@outlook.com>
2025-08-20 16:46:54 -0300

    animal

---
+++ conteudo/java02poo/aula08polimorfismo/ex02Animal/domain/Animal.java
@@ -0,0 +1,36 @@
+package projeto201.conteudo.java02poo.aula08polimorfismo.ex02Animal.domain;
+
+public abstract class Animal {
+    // atributos
+    protected String nome;
+    protected int idade;
+    protected double peso;
+
+    // construtor
+    public Animal(String nome, int idade, double peso) {
+        this.nome = nome;
+        this.idade = idade;
+        this.peso = peso;
+    }
+
+    // Metodos abstratos
+    public abstract void emitirSom();
+    public abstract void mover();
+    public abstract void alimentar();
+
+    // gancho para outras classes
+    protected abstract String detalhes();
+
+    // metodo exibir dados
+    public void exibirDados(){
+        System.out.println("""
+                Nome: %s
+                Idade: %s
+                Peso: %s
+                %s
+                """.formatted(nome,idade,peso, detalhes()));
+    }
+
+
+
+}
