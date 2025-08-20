69e6d7b76476a42d1c2e3517335102a9c39f4140
William Galvão <Liam_draven@outlook.com>
2025-08-20 16:47:11 -0300

    Create Peixe.java

---
+++ conteudo/java02poo/aula08polimorfismo/ex02Animal/domain/Peixe.java
@@ -0,0 +1,33 @@
+package projeto201.conteudo.java02poo.aula08polimorfismo.ex02Animal.domain;
+
+public class Peixe extends Animal{
+    // atributos
+    private String tipoAgua;
+
+    //construtor
+    public Peixe(String nome, int idade, double peso, String tipoAgua) {
+        super(nome, idade, peso);
+        this.tipoAgua = tipoAgua;
+    }
+
+    // metodos
+    @Override
+    public void emitirSom() {
+        System.out.println("Nenhum");
+    }
+
+    @Override
+    public void mover() {
+        System.out.println("Nada");
+    }
+
+    @Override
+    public void alimentar() {
+        System.out.println("ração/algas");
+    }
+
+    @Override
+    protected String detalhes() {
+        return "Veio de que tipo de água? "+tipoAgua;
+    }
+}
