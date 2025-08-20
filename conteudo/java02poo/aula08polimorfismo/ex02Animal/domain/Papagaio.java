e193de8f394ca96d22c773a95358a203d945f474
William Galvão <Liam_draven@outlook.com>
2025-08-20 16:47:13 -0300

    Create Papagaio.java

---
+++ conteudo/java02poo/aula08polimorfismo/ex02Animal/domain/Papagaio.java
@@ -0,0 +1,36 @@
+package projeto201.conteudo.java02poo.aula08polimorfismo.ex02Animal.domain;
+
+public class Papagaio extends Animal{
+    // atributos
+    private String corPenas;
+    private boolean sabeFalar;
+
+    // construtor
+    public Papagaio(String nome, int idade, double peso, String corPenas, boolean sabeFalar) {
+        super(nome, idade, peso);
+        this.corPenas = corPenas;
+        this.sabeFalar = sabeFalar;
+    }
+
+    // metodos
+    @Override
+    public void emitirSom() {
+        System.out.println("Canto/repetição de palavras");
+    }
+
+    @Override
+    public void mover() {
+        System.out.println("Voa");
+    }
+
+    @Override
+    public void alimentar() {
+        System.out.println("Sementes/Frutas");
+    }
+
+    @Override
+    protected String detalhes() {
+        return "Cor das penas:"+corPenas+
+                "| Sabe falar? "+(sabeFalar?"Sim":"Não");
+    }
+}
