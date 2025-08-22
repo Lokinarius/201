2fc5045ae620cc412364d5d9ec157fd89585ff3f
William Galvão <Liam_draven@outlook.com>
2025-08-22 20:44:11 -0300

    Update Retangulo.java

--- conteudo/java02poo/aula08polimorfismo/ex04figuras/domain/Retangulo.java
+++ conteudo/java02poo/aula08polimorfismo/ex04figuras/domain/Retangulo.java
@@ -1,4 +1,33 @@
 package projeto201.conteudo.java02poo.aula08polimorfismo.ex04figuras.domain;
 
-public class Retangulo {
+public class Retangulo extends Figura {
+    // atributos
+    private double base;
+    private double altura;
+
+    // construtor
+    public Retangulo(String cor, String nome, double base, double altura) {
+        super(cor, nome);
+        this.base = base;
+        this.altura = altura;
+    }
+
+    // metodos
+    @Override
+    public void calcularArea() {
+        System.out.println("Área: "+base*altura);
+    }
+
+    @Override
+    public void calcularPerimetro() {
+        System.out.println("Perímetro: "+2*(base+altura));
+    }
+
+    @Override
+    protected String detalhes() {
+        return """
+                Tamanho da base: %.2f
+                Tamanho da altura: %.2f
+                """.formatted(base,altura);
+    }
 }
