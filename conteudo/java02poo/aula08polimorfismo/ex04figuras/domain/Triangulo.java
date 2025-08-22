0e83f6a41f3c530ab2dc3f10b13a94fd860d4d9c
William Galvão <Liam_draven@outlook.com>
2025-08-22 20:44:16 -0300

    Update Triangulo.java

--- conteudo/java02poo/aula08polimorfismo/ex04figuras/domain/Triangulo.java
+++ conteudo/java02poo/aula08polimorfismo/ex04figuras/domain/Triangulo.java
@@ -1,4 +1,37 @@
 package projeto201.conteudo.java02poo.aula08polimorfismo.ex04figuras.domain;
 
-public class Triangulo {
+public class Triangulo extends Figura{
+    //atributos
+    private double base;
+    private double altura;
+    private double lado1,lado2,lado3;
+
+    // construtor
+    public Triangulo(String cor, String nome, double base, double altura, double lado1, double lado2, double lado3) {
+        super(cor, nome);
+        this.base = base;
+        this.altura = altura;
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+        this.lado3 = lado3;
+    }
+
+    // metodos
+    @Override
+    public void calcularArea() {
+        System.out.println("Área: "+(base*altura)/2);
+    }
+
+    @Override
+    public void calcularPerimetro() {
+        System.out.println("Perímetro: "+lado1+lado2+lado3);
+    }
+
+    @Override
+    protected String detalhes() {
+        return """
+                Base: %.2f
+                Altura: %.2f
+                """.formatted(base,altura);
+    }
 }
