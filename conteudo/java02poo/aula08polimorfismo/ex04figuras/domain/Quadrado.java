02b33cf8e39c1f2b8e5a39f93d6ed88ebbbd80a5
William Galvão <Liam_draven@outlook.com>
2025-08-22 20:44:05 -0300

    Update Quadrado.java

--- conteudo/java02poo/aula08polimorfismo/ex04figuras/domain/Quadrado.java
+++ conteudo/java02poo/aula08polimorfismo/ex04figuras/domain/Quadrado.java
@@ -1,4 +1,28 @@
 package projeto201.conteudo.java02poo.aula08polimorfismo.ex04figuras.domain;
 
-public class Quadrado {
+public class Quadrado extends Figura {
+    // atributos
+    private double lado;
+
+    // construtor
+    public Quadrado(String cor, String nome, double lado) {
+        super(cor, nome);
+        this.lado = lado;
+    }
+
+    // Metodos
+    @Override
+    public void calcularArea() {
+        System.out.println("Área: "+lado*lado);
+    }
+
+    @Override
+    public void calcularPerimetro() {
+        System.out.println("Perímetro: "+lado*4);
+    }
+
+    @Override
+    protected String detalhes() {
+        return "tamanho de um lado: "+lado;
+    }
 }
