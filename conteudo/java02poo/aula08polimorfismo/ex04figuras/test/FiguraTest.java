42deb70c6f909813b5c8b6126bfcdacc359e55e3
William Galvão <Liam_draven@outlook.com>
2025-08-22 20:44:24 -0300

    Update FiguraTest.java

--- conteudo/java02poo/aula08polimorfismo/ex04figuras/test/FiguraTest.java
+++ conteudo/java02poo/aula08polimorfismo/ex04figuras/test/FiguraTest.java
@@ -1,7 +1,11 @@
 package projeto201.conteudo.java02poo.aula08polimorfismo.ex04figuras.test;
 
+import projeto201.conteudo.java02poo.aula08polimorfismo.ex04figuras.domain.Figura;
+
+import java.util.ArrayList;
+
 public class FiguraTest {
     public static void main(String[] args) {
-        
+        ArrayList<Figura> figuras = new ArrayList<>();
     }
 }
