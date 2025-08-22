32abb3492413c52bc4c2c16cf9199e6c9b568daf
William Galvão <Liam_draven@outlook.com>
2025-08-22 20:43:58 -0300

    Update Figura.java

--- conteudo/java02poo/aula08polimorfismo/ex04figuras/domain/Figura.java
+++ conteudo/java02poo/aula08polimorfismo/ex04figuras/domain/Figura.java
@@ -1,4 +1,28 @@
 package projeto201.conteudo.java02poo.aula08polimorfismo.ex04figuras.domain;
 
-public class Figura {
+public abstract class Figura {
+    // Atributos
+    protected String cor;
+    protected String nome;
+
+    // Construtor
+    public Figura(String cor, String nome) {
+        this.cor = cor;
+        this.nome = nome;
+    }
+
+    // Metodos
+    public abstract void calcularArea();
+    public abstract void calcularPerimetro();
+
+    // gancho
+    protected abstract String detalhes();
+
+    public void exibirInfo(){
+        System.out.println("""
+                Nome: %s
+                Cor: %s
+                %s
+                """.formatted(nome,cor,detalhes()));
+    }
 }
