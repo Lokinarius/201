47c82dc860c6443ba66ced178fb8a8e1a9029880
William Galvão <Liam_draven@outlook.com>
2025-08-23 18:44:22 -0300

    update

--- conteudo/java02poo/aula08polimorfismo/ex04figuras/domain/Circulo.java
+++ conteudo/java02poo/aula08polimorfismo/ex04figuras/domain/Circulo.java
@@ -2,5 +2,29 @@ package projeto201.conteudo.java02poo.aula08polimorfismo.ex04figuras.domain;
 
 public class Circulo extends Figura{
     // atributos
+    private double raio;
 
+    // construtor
+    public Circulo(String cor, String nome, double raio) {
+        super(cor, nome);
+        this.raio = raio;
+    }
+
+    // metodos
+    @Override
+    public void calcularArea() {
+        System.out.println("Área: "+Math.PI * Math.pow(raio,2));
+    }
+
+    @Override
+    public void calcularPerimetro() {
+        System.out.println("Perímetro: "+2*Math.PI*raio);
+    }
+
+    @Override
+    protected String detalhes() {
+        return """
+                Raio: %.2f
+                """.formatted(raio);
+    }
 }
--- conteudo/java02poo/aula08polimorfismo/ex04figuras/test/FiguraTest.java
+++ conteudo/java02poo/aula08polimorfismo/ex04figuras/test/FiguraTest.java
@@ -1,11 +1,24 @@
 package projeto201.conteudo.java02poo.aula08polimorfismo.ex04figuras.test;
 
-import projeto201.conteudo.java02poo.aula08polimorfismo.ex04figuras.domain.Figura;
+import projeto201.conteudo.java02poo.aula08polimorfismo.ex04figuras.domain.*;
 
 import java.util.ArrayList;
 
 public class FiguraTest {
     public static void main(String[] args) {
         ArrayList<Figura> figuras = new ArrayList<>();
+
+        figuras.add(new Retangulo("Vermelho", "Retângulo 1", 5, 10));
+        figuras.add(new Quadrado("Azul", "Quadrado 1", 7));
+        figuras.add(new Triangulo("Verde", "Triângulo 1", 6, 8, 5,5,9));
+        figuras.add(new Circulo("Amarelo", "Círculo 1", 4));
+
+
+        for(Figura figura: figuras){
+            figura.exibirInfo();
+            figura.calcularArea();
+            figura.calcularPerimetro();
+            System.out.println("------------------------");
+        }
     }
 }
