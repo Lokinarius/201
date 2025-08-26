ea29661f2da061a03c5e7cd642b5beec8f8212de
William Galvão <Liam_draven@outlook.com>
2025-08-26 18:01:40 -0300

    Create EntradaInvalidaException.java

---
+++ conteudo/java03avancado/aula01excecoes/ex02validacao/exception/EntradaInvalidaException.java
@@ -0,0 +1,7 @@
+package projeto201.conteudo.java03avancado.aula01excecoes.ex02validacao.exception;
+
+public class EntradaInvalidaException extends Exception {
+    public EntradaInvalidaException(String mensagem){
+        super(mensagem);
+    }
+}
