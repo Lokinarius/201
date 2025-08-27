db041624a4d3c6aae9325ef6a57ca8aa8839aff3
William Galvão <Liam_draven@outlook.com>
2025-08-27 17:59:26 -0300

    Create Main.java

---
+++ conteudo/java03avancado/aula01excecoes/ex03validadoridade/app/Main.java
@@ -0,0 +1,9 @@
+package projeto201.conteudo.java03avancado.aula01excecoes.ex03validadoridade.app;
+
+import projeto201.conteudo.java03avancado.aula01excecoes.ex03validadoridade.ui.ConsoleUI;
+
+public class Main {
+    public static void main(String[] args) {
+        new ConsoleUI().iniciar();
+    }
+}
