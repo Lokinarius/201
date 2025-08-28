1763f24783a35a6662201dbf4bba2c94327cd431
William Galvão <Liam_draven@outlook.com>
2025-08-28 18:35:33 -0300

    Create InvalidOperatorexception.java

---
+++ conteudo/java03avancado/aula01excecoes/ex05calculadora2/exception/InvalidOperatorexception.java
@@ -0,0 +1,7 @@
+package projeto201.conteudo.java03avancado.aula01excecoes.ex05calculadora2.exception;
+
+public class InvalidOperatorexception extends CalculadoraException{
+    public InvalidOperatorexception(String message) {
+        super(message);
+    }
+}
