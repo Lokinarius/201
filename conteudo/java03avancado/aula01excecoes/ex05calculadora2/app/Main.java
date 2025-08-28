285eb871d385289fa3b3af460b1458bb6a50e3be
William Galvão <Liam_draven@outlook.com>
2025-08-28 18:35:18 -0300

    Create Main.java

---
+++ conteudo/java03avancado/aula01excecoes/ex05calculadora2/app/Main.java
@@ -0,0 +1,10 @@
+package projeto201.conteudo.java03avancado.aula01excecoes.ex05calculadora2.app;
+
+import projeto201.conteudo.java03avancado.aula01excecoes.ex05calculadora2.ui.UserInterface;
+
+public class Main {
+    public static void main(String[] args) {
+        UserInterface ui = new UserInterface();
+        ui.start();
+    }
+}
