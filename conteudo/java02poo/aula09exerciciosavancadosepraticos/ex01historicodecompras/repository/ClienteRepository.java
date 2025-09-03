2744cf086c7c568681046e9104502905a164d975
William Galvão <Liam_draven@outlook.com>
2025-09-02 22:21:44 -0300

    a

--- conteudo/java02poo/aula09exerciciosavancadosepraticos/ex01historicodecompras/repository/ClienteRepository.java
+++ conteudo/java02poo/aula09exerciciosavancadosepraticos/ex01historicodecompras/repository/ClienteRepository.java
@@ -1,4 +1,9 @@
 package projeto201.conteudo.java02poo.aula09exerciciosavancadosepraticos.ex01historicodecompras.repository;
 
+import projeto201.conteudo.java02poo.aula09exerciciosavancadosepraticos.ex01historicodecompras.domain.Cliente;
+
 public class ClienteRepository {
+    public void salvar(){
+
+    }
 }
--- conteudo/java02poo/aula09exerciciosavancadosepraticos/ex01historicodecompras/repository/ProdutoRepository.java
+++ conteudo/java02poo/aula09exerciciosavancadosepraticos/ex01historicodecompras/repository/ProdutoRepository.java
@@ -1,4 +1,32 @@
 package projeto201.conteudo.java02poo.aula09exerciciosavancadosepraticos.ex01historicodecompras.repository;
 
+import projeto201.conteudo.java02poo.aula09exerciciosavancadosepraticos.ex01historicodecompras.domain.Produto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProdutoRepository {
+    private List<Produto> produtos = new ArrayList<>();
+
+    public void salvar(Produto p){
+        produtos.add(p);
+    }
+
+    public List<Produto> listarTodos() {
+        return produtos;
+    }
+
+    public Produto buscarporId(int id){
+        return produtos.stream().filter(p -> p.getId() == id).findFirst().orElse(null);
+
+    }
+
+    public void remover(int id){
+        produtos.removeIf(p -> p.getId() == id);
+    }
+
+    public void atualizar(Produto p){
+        remover(p.getId());
+        salvar(p);
+    }
 }
