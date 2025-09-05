f9698a0a85945de0b0dfe28b6db6a4335c140ff4
William Galvão <Liam_draven@outlook.com>
2025-09-05 14:25:15 -0300

    Create TestWrapper.java

---
+++ conteudo/java03avancado/aula02classesutilitarias/parte01lang/Ex02Wrapper/TestWrapper.java
@@ -0,0 +1,32 @@
+package projeto201.conteudo.java03avancado.aula02classesutilitarias.parte01lang.Ex02Wrapper;
+
+import java.util.Scanner;
+
+public class TestWrapper {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ServiceWrapper service = new ServiceWrapper();
+        int opcao;
+
+        do{
+            System.out.println("""
+                    \n === WRAPPER ===
+                    1 - Boxing e Unboxing
+                    2 - Conversões (String e Número)
+                    3 - Uso em coleções
+                    0 - Sair
+                    """);
+            opcao = sc.nextInt();
+
+            switch (opcao){
+                case 1 -> service.boxingUnboxing();
+                case 2 -> service.conversao();
+                case 3 -> service.colecoes();
+                case 0 -> System.out.println("Saindo...");
+                default -> System.out.println("Opção inválida");
+            }
+        }while (opcao != 0);
+
+        sc.close();
+    }
+}
