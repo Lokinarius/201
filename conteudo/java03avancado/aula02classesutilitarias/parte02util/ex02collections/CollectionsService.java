4b76b926341a140ceddf3588e6ab8290b3c357de
William Galvão <Liam_draven@outlook.com>
2025-09-13 22:55:22 -0300

    Create CollectionsService.java

---
+++ conteudo/java03avancado/aula02classesutilitarias/parte02util/ex02collections/CollectionsService.java
@@ -0,0 +1,34 @@
+package projeto201.conteudo.java03avancado.aula02classesutilitarias.parte02util.ex02collections;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class CollectionsService {
+    private Scanner sc = new Scanner(System.in);
+
+    // 1.Ordenar lista
+    public void ordenarLista() {
+        List<String> nomes
+    }
+
+    public void buscarElementos() {
+    }
+
+    public void encontrarMaxMin() {
+    }
+
+    public void substituirElementos() {
+    }
+
+    public void preecherLista() {
+    }
+
+    public void copiarLista() {
+    }
+
+    public void embaralharLista() {
+    }
+
+    public void inverterLista() {
+    }
+}
