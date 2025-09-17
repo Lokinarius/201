1396b540a8cabfd67cccfcd65f6050c7e2eed70e
William Galvão <Liam_draven@outlook.com>
2025-09-17 16:59:30 -0300

    Update DurationService.java

--- conteudo/java03avancado/aula02classesutilitarias/parte03time/ex02duration/DurationService.java
+++ conteudo/java03avancado/aula02classesutilitarias/parte03time/ex02duration/DurationService.java
@@ -1,7 +1,6 @@
 package projeto201.conteudo.java03avancado.aula02classesutilitarias.parte03time.ex02duration;
 
 import java.time.Duration;
-import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Scanner;
 
@@ -27,18 +26,33 @@ public class DurationService {
         System.out.println("Digite hora e minuto final (ex: 16 45): ");
         LocalTime fim = LocalTime.of(sc.nextInt(), sc.nextInt());
 
-        Duration d = Duration
+        Duration d = Duration.between(inicio,fim);
+        System.out.println("Duração entre os horários: " + d);
+        System.out.println("Em minutos: " + d.toMinutes());
     }
 
     // 3.adicionar tempo a uma duração
     public void adicionarTempo() {
+        Duration d = Duration.ofMinutes(90);
+        System.out.println("Duração original: " + d);
+        Duration nova = d.plusMinutes(30).plusSeconds(45);
+        System.out.println("Após adicionar 30min e 45s: " + nova);
     }
 
     // 4.Substituir tempo de uma duração
     public void subtrairTempo() {
+        Duration d = Duration.ofHours(2);
+        System.out.println("Duration original: " + d);
+        Duration nova = d.minusMinutes(45);
+        System.out.println("Após subtrair 45min: " + nova);
     }
 
     // 5.Converter duração
     public void converterDuracao() {
+        Duration d = Duration.ofSeconds(3675);
+        System.out.println("Duração: " + d);
+        System.out.println("Em horas: " + d.toHours());
+        System.out.println("Em minutos: " + d.toMinutes());
+        System.out.println("Em segundos: " + d.toSeconds());
     }
 }
