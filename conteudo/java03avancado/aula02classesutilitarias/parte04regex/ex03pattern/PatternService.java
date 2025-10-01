91471a838bdc3cefcfebfae25d29e0e65cd9dfd4
William Galvão <Liam_draven@outlook.com>
2025-09-30 22:44:52 -0300

    Create PatternService.java

---
+++ conteudo/java03avancado/aula02classesutilitarias/parte04regex/ex03pattern/PatternService.java
@@ -0,0 +1,24 @@
+package projeto201.conteudo.java03avancado.aula02classesutilitarias.parte04regex.ex03pattern;
+
+import java.util.Scanner;
+
+public class PatternService {
+    private Scanner sc = new Scanner(System.in);
+
+    // 1. Validar email
+    public void validarEmail() {
+        System.out.println("Digite");
+    }
+
+    public void validarCPF() {
+    }
+
+    public void validarSenhaForte() {
+    }
+
+    public void encontrarNumerosTexto() {
+    }
+
+    public void encontrarPalavrasMaiusculas() {
+    }
+}
