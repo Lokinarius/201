8caa4e3cd6ff378ae73b69996c894ddb17529bee
William Galvão <Liam_draven@outlook.com>
2025-10-06 21:36:13 -0300

    Create RegexScannerService.java

---
+++ conteudo/java03avancado/aula02classesutilitarias/parte04regex/ex04scanner/RegexScannerService.java
@@ -0,0 +1,22 @@
+package projeto201.conteudo.java03avancado.aula02classesutilitarias.parte04regex.ex04scanner;
+
+public class RegexScannerService {
+    
+
+    public void lerPalavras() {
+    }
+
+    public void lerNumeros() {
+    }
+
+    public void lerTokensVirgula() {
+    }
+
+    public void lerEmails() {
+    }
+
+    public void lerValoresMonetarios() {
+    }
+
+
+}
